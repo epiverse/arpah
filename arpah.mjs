f8adcc9125a48568313607be997c0592f99e586c
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-09 04:25:16 -0400

    resume

--- arpah.mjs
+++ arpah.mjs
@@ -1,8 +1,28 @@
 console.log(`arphah.mjs loaded ${Date()}`)
 
+// hello
 const arpah={}
 arpah.hello='world'
 arpah.loadedAt=Date()
+
+// Yjs, notes and signaling:
+// https://sites.google.com/mathbiol.org/cloud4bio/2024/2024-05-01-may
+
+class YJS {
+    constructor(sigServer='https://yjs-demo-signaling-server-jonasalmeida.replit.app/'){
+        this.loadedAt=Date()
+        this.sigStatus = async function(sigServer){
+            return await (await fetch(sigServer)).text()
+        }
+        console.log(`new Yjs object created at\n${this.loadedAt}`)
+        4
+    }
+}
+
+
+
+
 export{
-    arpah
+    arpah,
+    YJS
 }
