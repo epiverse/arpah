531f5cf685a3581a12c41f9e79e763f9d2d9f851
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-09 05:51:24 -0400

    debug to work in obs

--- arpah.mjs
+++ arpah.mjs
@@ -9,10 +9,18 @@ arpah.loadedAt=Date()
 // https://sites.google.com/mathbiol.org/cloud4bio/2024/2024-05-01-may
 
 class YJS {
-    constructor(sigServer='https://yjs-demo-signaling-server-jonasalmeida.replit.app/'){
+    constructor(sigServerURL){
         this.loadedAt=Date()
-        this.sigStatus = async function(sigServer){
-            return await (await fetch(sigServer)).text()
+        this.sigStatus = async function(sigServerURL){
+            if(!sigServerURL){sigServerURL='https://yjs-demo-signaling-server-jonasalmeida.replit.app/'}
+            console.log('signaling server address:',sigServerURL)
+            let res = await (await fetch(sigServerURL)).text()
+            console.log('response:',res) // in case there are other responses
+            if(res=='okay'){
+                return true
+            }else{
+                return false
+            }
         }
         console.log(`new Yjs object created at\n${this.loadedAt}`)
         4
